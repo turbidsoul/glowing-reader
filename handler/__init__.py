6ea3849a2b1347be4f17956e68a0886c1a77aa70
turbidsoul <sccn.sq@gmail.com>
2013-08-29 23:40:01 +0800

    update base.html and main.html

--- handler/__init__.py
+++ handler/__init__.py
@@ -2,7 +2,7 @@
 
 
 from tornado.web import RequestHandler
-from settings import jinja2_env
+from settings import jinja2_env, title, wwwroot
 
 
 class BaseHandler(RequestHandler):
@@ -12,4 +12,6 @@ class BaseHandler(RequestHandler):
 
     def render_string(self, template_name, **kwargs):
         template = jinja2_env.get_template(template_name)
+        kwargs['title'] = title
+       	kwargs['wwwroot'] = wwwroot
         self.write(template.render(**kwargs))
--- settings.py
+++ settings.py
@@ -5,7 +5,8 @@ from jinja2 import Environment, FileSystemLoader
 
 base_path = os.path.split(os.path.realpath(__file__))[0]
 
-title="Glowing Reader"
+title = "Glowing Reader"
+wwwroot = "http://127.0.0.1:8888/"
 template_path = os.path.join(base_path, "template")
 db_file = os.path.join(base_path, 'db.sqlite')
 static_path = os.path.join(base_path, 'static')
@@ -13,3 +14,4 @@ static_path = os.path.join(base_path, 'static')
 debug = True
 
 jinja2_env = Environment(loader=FileSystemLoader(template_path), auto_reload=debug)
+
