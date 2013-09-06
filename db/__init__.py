fa2b6063827951e531a2d54733b09984e4eaf460
Turbidsoul Chen <sccn.sq@gmail.com>
2013-09-06 18:47:08 +0800

    update models.py

--- db/__init__.py
+++ db/__init__.py
@@ -3,10 +3,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-import settings
 
-engine = create_engine("sqlite:///%s" % settings.db_file, echo=True)
-Base = declarative_base(bind=engine)
-Session = sessionmaker()
-Session.configure(bind=engine)
-session = Session()
+
+_engine = create_engine("sqlite:///:memory:", echo=True)
+Base = declarative_base(bind=_engine)
+_Session = sessionmaker()
+_Session.configure(bind=_engine)
+session = _Session()
+
+
+class Model(Base):
+    """Base Model"""
+
+    def save(self):
+        return session.add(self)
+
+    @classmethod
+    def find_all(cls):
+        return session.query(cls).all()
--- db/models.py
+++ db/models.py
@@ -1,11 +1,19 @@
 # -*- coding: utf8 -*-
 
 from db import Model
+from sqlalchemy import Column, String, Sequence, Integer
 
 
 class Feed(Model):
     """Feed Model"""
-    
+    __tablename__ = 'feed'
+    id = Column(Integer, Sequence('feed_id_seq'), primary_key=True)
+    title = Column(String)
+    link = Column(String(255))
+
+    def __init__(self, title, link):
+        self.title = title
+        self.link = link
 
 
 class User(Model):
