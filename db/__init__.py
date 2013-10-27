f477aecee7376b2c7a76742ea7f5b293216be22c
turbidsoul <sccn.sq@gmail.com>
2013-10-27 23:56:50 +0800

    update db module

--- db/__init__.py
+++ db/__init__.py
@@ -1,25 +1,83 @@
 # -*- coding: utf8 -*-
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
 _engine = create_engine("sqlite:///:memory:", echo=True)
-Base = declarative_base(bind=_engine)
-_Session = sessionmaker()
-_Session.configure(bind=_engine)
-_session = _Session()
+
+def init_db():
+    Base.metadata.create_all(_engine)
+
+def drop_db():
+    Base.metadata.drop_all(_engine)
 
 
-class Model(Base):
-    """Base Model"""
-    def save(self):
-        return _session.add(self)
+class Model(object):
+
+    def save(self, session):
+        session.add(self)
+        session.commit()
+        return self
+
+    def update(self, session):
+        session.update(self)
+        session.commit()
+        return self;
 
     @classmethod
-    def find_all(cls):
-        return _session.query(cls).all()
+    def find_all(cls, session, columns=None, offset=None, limit=None,
+                 order_by=None, lock_mode=None):
+        query = session.query(cls)
+        if columns:
+            query.values(columns)
+        if offset:
+            query.offset(offset)
+        if limit:
+            query.limit(limit)
+        if order_by:
+            query.order_by(*order_by)
+        if lock_mode:
+            query.with_lockmode(lock_mode)
+        return session.all()
 
-    def delete(self):
-        return _session.delete(self)
+    @classmethod
+    def find(cls, session, columns=None, offset=None, limit=None, order_by=None,
+             lock_mode=None, **kwargs):
+        query = session.query(cls)
+        if columns:
+            query.values(columns)
+        if offset:
+            query.offset(offset)
+        if limit:
+            query.limit(limit)
+        if order_by:
+            query.order_by(*order_by)
+        if lock_mode:
+            query.with_lockmode(lock_mode)
+        if kwargs:
+            query.filter_by(**kwargs)
+        return query.all()
+
+    @classmethod
+    def count(cls, session, lock_mode=None, **kwargs):
+        query = session.query(cls)
+        if kwargs:
+            query.filter_by(**kwargs)
+        if lock_mode:
+            query.with_lockmode(lock_mode)
+        return query.count()
+
+    @classmethod
+    def exists(cls, session, lock_mode=None, **kwargs):
+        query = session.query(cls)
+        if kwargs:
+            query.filter_by(**kwargs)
+        if lock_mode:
+            query.with_lockmode(lock_mode)
+        return session.query(query.exists()).scalar()
+
+
+
+Base = declarative_base(cls=Model)
+Session = sessionmaker(bind=_engine)
--- db/models.py
+++ db/models.py
@@ -1,26 +1,40 @@
 # -*- coding: utf8 -*-
 
-from db import Model
-from sqlalchemy import Column, String, Sequence, Integer
+from db import Base
+from datetime import datetime
+from sqlalchemy import Column, String, Sequence, Integer, DateTime
 
 
-class Feed(Model):
+class Feed(Base):
     """Feed Model"""
     __tablename__ = 'feed'
-    id = Column(Integer, Sequence('feed_id_seq'), primary_key=True)
+    feed_id = Column(Integer, Sequence('feed_id_seq'), primary_key=True)
     title = Column(String)
+    url = Column(String(255))
     link = Column(String(255))
+    last_update_time = Column(DateTime)
+    create_time = Column(DateTime)
 
-    def __init__(self, title, link):
+
+    def __init__(self, title, url, link):
         self.title = title
+        self.url = url
         self.link = link
+        self.create_time = datetime.now()
 
 
-class User(Model):
+
+class User(Base):
     """User Model"""
-    pass
+    __tablename__ = 'user'
+    user_id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    user_name = Column(String(50))
+    password = Column(String)
 
 
-class Tag(Model):
+class Tag(Base):
     """Tag Model"""
-    pass
+    __tablename__ = 'tag'
+    tag_id = Column(Integer, Sequence('tag_id_seq'), primary_key=True)
+    tag_name = Column(String(50))
+    count = Column(Integer)
