f93f686e42fdfd7115a9ee98c62776078c3b4e2e
turbidsoul <sccn.sq@gmail.com>
2013-11-01 23:38:36 +0800

    合并reader到main中

--- handler/main.py
+++ handler/main.py
@@ -2,6 +2,7 @@
 
 
 from handler import BaseHandler
+from feed import get_feed
 
 
 class MainHandler(BaseHandler):
@@ -33,3 +34,14 @@ class AboutHandler(BaseHandler):
     """
     def get(self):     
         return self.render("about.html", page='about')
+
+
+class ReaderHandler(BaseHandler):
+    def get(self, url=None):
+        d = get_feed("http://blog.turbidsoul.me/rss.xml")
+        return self.render('reader.html', feed=d.feed, entries=d.entries, page='reader')
+
+
+class AddRssHandler(BaseHandler):
+    def post(self, url, tag=None):
+        pass
