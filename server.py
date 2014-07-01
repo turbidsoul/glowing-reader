8549aa52909c0e4bbade4dda861066d9db045152
Turbidsoul <sccn.sq@gmail.com>
2014-07-01 17:44:29 +0800

    update db

--- server.py
+++ server.py
@@ -1,31 +1,65 @@
 # -*- coding: utf8 -*-
 
-from bottle import default_app, jinja2_template as template, Bottle, TEMPLATE_PATH
+import settings
+from bottle import jinja2_template as template
+from bottle import default_app, install, route, run, TEMPLATE_PATH, abort
 from bottle.ext import sqlite
-
-# import settings
+import traceback
 
 TEMPLATE_PATH.insert(0, './template/')
-app = Bottle()
-app.install(sqlite.Plugin(dbfile=':memory:'))
+install(sqlite.Plugin(dbfile='./db.sqlite'))
 
 
 def renderer(f, **kwargs):
     params = {
-        'title': 'Fire Reader'
+        'title': settings.title
     }
     params.update(kwargs)
     return template(f, params)
 
 
 
-@app.route('')
-@app.route('/')
-def index():
-    return renderer('index.html')
+@route('')
+@route('/')
+def index(db):
+    cursor = db.execute("SELECT * FROM feed")
+    feeds = cursor.fetchall()
+    return renderer('index.html', feeds=feeds)
+
+
+
+
+@route('/init')
+def init(db):
+    try:
+
+        db.executescript('''CREATE TABLE IF NOT EXISTS "feed" (
+            "id" INTEGER PRIMARY KEY autoincrement,
+            "site_name" VARCHAR NOT NULL,
+            "site_url" VARCHAR NOT NULL,
+            "feed_url" VARCHAR NOT NULL,
+            "last_update" DATETIME NOT NULL,
+            "create_time" DATETIME NOT NULL
+        );''')
+
+        db.executescript('''CREATE TABLE IF NOT EXISTS "feed_item" (
+            "id" INTEGER PRIMARY KEY autoincrement,
+            "name" VARCHAR NOT NULL,
+            "url" VARCHAR NOT NULL,
+            "title" VARCHAR NOT NULL,
+            "author" VARCHAR NOT NULL,
+            "description" VARCHAR,
+            "publish_date" DATETIME NOT NULL,
+            "create_time" DATETIME NOT NULL
+        );''')
+        db.commit()
+    except Exception:
+        traceback.print_exc()
+        abort(500, traceback.format_exc())
+    return "初始化成功"
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=9000, reloader=True, debug=True)
+    run(host='127.0.0.1', port=9000, reloader=True, debug=True)
 else:
     application = default_app()
